e0e0d6c4407efa0cf10c2c8ace779bfe03b10cd0
biggiemedium <78000142+biggiemedium@users.noreply.github.com>
2023-05-12 11:01:06 -0400

    UI update 2

---
+++ src/Render/Game/GameFrame.java
@@ -0,0 +1,4 @@
+package Render.Game;
+
+public class GameFrame {
+}
--- src/Render/MainMenu.java
+++ src/Render/MainMenu.java
@@ -3,6 +3,7 @@ package Render;
 import Util.Util;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,13 +13,16 @@ public class MainMenu extends JFrame implements ActionListener, Util {
     private JPanel panel;
     private JButton start;
     private JButton exit;
+    private JTextField war;
 
     public MainMenu() {
         this.frame = new JFrame("War");
         this.panel = new JPanel(null);
         this.start = new JButton("Start");
         this.exit = new JButton("Exit");
+        this.war = new JTextField("War");
         handleFrame();
+        handleComponents();
     }
 
     public void handleFrame() {
@@ -34,9 +38,16 @@ public class MainMenu extends JFrame implements ActionListener, Util {
         this.panel.setSize(frame.getWidth(), frame.getHeight());
         this.panel.add(start);
         this.panel.add(exit);
+        this.panel.add(war);
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void handleButtons() {
+    public void handleComponents() {
+        this.exit.setBounds(panel.getWidth() / 2 - 100, panel.getHeight() / 2 + 50, 200, 75);
         this.exit.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -44,12 +55,16 @@ public class MainMenu extends JFrame implements ActionListener, Util {
             }
         });
 
+        this.start.setBounds(panel.getWidth() / 2 - 125, panel.getHeight() / 2 - 50, 250, 100);
         this.start.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Add gameStart
             }
         });
+
+        this.war.setBounds(panel.getWidth() / 2 - 125, 25, 250, 100);
+
     }
 
     @Override
